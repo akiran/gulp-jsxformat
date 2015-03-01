7544a36c4b71fa3f5de9cc9e9f1f56f277634796
kiran <kiran@webrafter.com>
2015-03-01 11:07:45 +0530

    Added basic code for gulp jsx formating

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var jsxformat = require('jsxformat');
+var gutil = require('gulp-util');
+var through = require('through2');
+
+module.exports = function (options) {
+  options = options || {};
+  return through.obj(function (file, encoding, callback) {
+    try {
+      var str = file.contents.toString(encoding || 'utf8');
+      file.contents = new Buffer(jsxformat(str));
+    } catch (err) {
+      return callback(new gutil.PluginError('gulp-jsxformat', err, options));
+    }
+    callback(null, file);
+  });
+};
